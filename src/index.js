941d45057ca9484c00b3c5ef656cce702e91f0c6
Adrian P. Cadag <adriancadag@Adrians-MacBook-Air.local>
2018-12-29 02:35:37 +0800

    Added sample http call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import './index.css';
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
-debugger; // eslint-disable-line no-debugger
-console.log(`I would pay ${courseValue} for this awsome course!`); // eslint-disable-line no-console
+// Populate table of users via API call.
+getUsers().then(result => {
+  let userBody = "";
+
+  result.forEach(user => {
+    userBody += `<tr>
+      <td><a href="#" data-id = "${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      <td>
+    </tr>`
+  });
+
+  document.getElementById('users').innerHTML = userBody;
+});
+
+//import numeral from 'numeral';
+
+//const courseValue = numeral(1000).format('$0,0.00');
+//debugger; eslint-disable-line no-debugger
+//console.log(`I would pay ${courseValue} for this awsome course!`); // eslint-disable-line no-console
